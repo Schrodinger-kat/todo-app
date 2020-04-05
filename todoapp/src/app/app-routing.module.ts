5feb94a47fd2733a1a5c1e0e32952d1cec361a8c
jishnn tp <jishnn.tp@gmail.com>
2020-04-05 12:44:42 +0530

    final commit

--- todoapp/src/app/app-routing.module.ts
+++ todoapp/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { TodoComponent } from './Components/todo/todo.component';
+import { from } from 'rxjs';
+import {AboutComponent} from './Components/pages/about/about.component';
+const routes: Routes = [
+  { path: '',component: TodoComponent },
+  { path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- todoapp/src/app/app.module.ts
+++ todoapp/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { HeaderComponent } from './Components/layout/header/header.component';
 import { AddTodoComponent } from './Components/add-todo/add-todo.component';
 import { FormsModule} from '@angular/forms';
 import { from } from 'rxjs';
+import { AboutComponent } from './Components/pages/about/about.component';
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
     TodoItemComponent,
     HeaderComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
